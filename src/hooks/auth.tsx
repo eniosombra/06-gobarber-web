424157feb791b8323664bf05cc3c7c9e2f5738df
Enio L. Sombra <eniosombra@gmail.com>
2020-05-27 11:40:51 -0300

    Criação da Page Profile

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,6 +9,7 @@ interface SignInCredencials {
 interface User {
   id: string;
   name: string;
+  email: string;
   avatar_url: string;
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,11 @@ import { Switch } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 import Route from './Route';
 
@@ -18,6 +20,7 @@ const Routes: React.FC = () => {
       <Route path="/reset-password" component={ResetPassword} />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 };
